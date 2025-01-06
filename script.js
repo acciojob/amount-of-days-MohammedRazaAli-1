8bffe5b0fc4a549af2381a959694392a7c4f261f
MohammedRazaAli-1 <144483340+MohammedRazaAli-1@users.noreply.github.com>
2025-01-06 12:34:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,14 +2,14 @@
 function daysOfAYear(year){
 	
 	if(year %4 ==0 && year %100 !=0){
-		return "leap year";
+		return "366";
 	}else{
-		return "not a leap year"
+		return "365"
 	}
 }
 
 
 
- let inputyear =prompt("enter year") 
+ //let inputyear =prompt("enter year") 
 let answer = daysOfAYear(inputyear)
 alert(answer)
